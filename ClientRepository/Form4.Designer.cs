d3e7960201434894ecf07e92a9d1659908de74f9
Amy Chada <amy.chada@gmail.com>
2026-01-20 02:46:46 +0000

    UPDAATE

---
+++ ClientRepository/Form4.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ClientRepository
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LblLogin = new Label();
+            LblUsername = new Label();
+            TxtUsername = new TextBox();
+            LblPassword = new Label();
+            TxtPassword = new TextBox();
+            BtnLogin = new Button();
+            SuspendLayout();
+            // 
+            // LblLogin
+            // 
+            LblLogin.AutoSize = true;
+            LblLogin.Location = new Point(64, 52);
+            LblLogin.Name = "LblLogin";
+            LblLogin.Size = new Size(56, 25);
+            LblLogin.TabIndex = 0;
+            LblLogin.Text = "Login";
+            // 
+            // LblUsername
+            // 
+            LblUsername.AutoSize = true;
+            LblUsername.Location = new Point(61, 107);
+            LblUsername.Name = "LblUsername";
+            LblUsername.Size = new Size(95, 25);
+            LblUsername.TabIndex = 1;
+            LblUsername.Text = "Username:";
+            // 
+            // TxtUsername
+            // 
+            TxtUsername.Location = new Point(197, 107);
+            TxtUsername.Name = "TxtUsername";
+            TxtUsername.Size = new Size(150, 31);
+            TxtUsername.TabIndex = 2;
+            // 
+            // LblPassword
+            // 
+            LblPassword.AutoSize = true;
+            LblPassword.Location = new Point(61, 171);
+            LblPassword.Name = "LblPassword";
+            LblPassword.Size = new Size(91, 25);
+            LblPassword.TabIndex = 3;
+            LblPassword.Text = "Password:";
+            // 
+            // TxtPassword
+            // 
+            TxtPassword.Location = new Point(197, 171);
+            TxtPassword.Name = "TxtPassword";
+            TxtPassword.Size = new Size(150, 31);
+            TxtPassword.TabIndex = 4;
+            // 
+            // BtnLogin
+            // 
+            BtnLogin.Location = new Point(61, 357);
+            BtnLogin.Name = "BtnLogin";
+            BtnLogin.Size = new Size(112, 34);
+            BtnLogin.TabIndex = 5;
+            BtnLogin.Text = "Login";
+            BtnLogin.UseVisualStyleBackColor = true;
+            BtnLogin.Click += BtnLogin_Click;
+            // 
+            // Form4
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(BtnLogin);
+            Controls.Add(TxtPassword);
+            Controls.Add(LblPassword);
+            Controls.Add(TxtUsername);
+            Controls.Add(LblUsername);
+            Controls.Add(LblLogin);
+            Name = "Form4";
+            Text = "Form4";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label LblLogin;
+        private Label LblUsername;
+        private TextBox TxtUsername;
+        private Label LblPassword;
+        private TextBox TxtPassword;
+        private Button BtnLogin;
+    }
+}
---
+++ ClientRepository/Form4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClientRepository
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+        private Dictionary<string, string> clients = new Dictionary<string, string>();
+        private Dictionary<string, string> admins = new Dictionary<string, string>();
+        private Dictionary<string, string> Staff = new Dictionary<string, string>();
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string username = TxtUsername.Text;
+            string password = TxtPassword.Text;
+
+            if (AuthenticateStaff(username, password))
+            {
+                MessageBox.Show("Login successful!");
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.");
+            }
+        }
+        private bool AuthenticateStaff(string username, string password)
+        {
+            return Staff.ContainsKey(username) && Staff[username] == password;
+        }
+    }
+}
